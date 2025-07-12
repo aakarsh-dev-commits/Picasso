c564cb68fa81f312ef39b5647a79a11feee3e36a
Aakarsh Srivastava <138319551+aakarsh-dev-commits@users.noreply.github.com>
2025-07-12 13:43:22 +0530

    Add eventlistners for color changing functionality

--- javascript.js
+++ javascript.js
@@ -21,19 +21,19 @@ slider.oninput = function () {
 
 const color = document.getElementById("colorpicker");
 let colorPicked = color.value;
+
 color.oninput = function () {
   colorPicked = this.value;
-  grid.innerHTML = "";
-  squares.length = 0;
-  squareContainers.length = 0;
-  updateGrid();
 };
+
+const normal = document.querySelector(".Normal");
+const clear = document.querySelector(".Clear");
 
 const squareContainers = [];
 const squares = [];
 
 function updateGrid() {
-  const sqaureSize = 500 / sliderVal - 2;
+  const sqaureSize = 500 / sliderVal - 1;
   for (let i = 0; i < sliderVal; i++) {
     const sqaureContainer = document.createElement("div");
     grid.appendChild(sqaureContainer);
@@ -48,14 +48,34 @@ function updateGrid() {
 
       sqaure.style.width = `${sqaureSize}px`;
       sqaure.style.height = `${sqaureSize}px`;
-      sqaure.style.backgroundColor = colorPicked;
+      sqaure.style.backgroundColor = "#ffffff";
       sqaure.style.borderStyle = "solid";
-      sqaure.style.borderWidth = "1px";
+      sqaure.style.borderWidth = "0.5px";
       sqaure.style.borderColor = "black";
 
       squares.push(sqaure);
     }
   }
+
+  normal.addEventListener("click", () => {
+    squares.forEach((sqaure) => {
+      sqaure.addEventListener("pointerdown", () => {
+        sqaure.style.backgroundColor = colorPicked;
+      });
+      sqaure.addEventListener("pointermove", (event) => {
+        if (event.buttons === 1) {
+          sqaure.style.backgroundColor = colorPicked;
+        }
+      });
+    });
+  });
+
+  clear.addEventListener("click", () => {
+    grid.innerHTML = "";
+    squares.length = 0;
+    squareContainers.length = 0;
+    updateGrid();
+  });
 }
 
 updateGrid();
